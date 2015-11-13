50813cecbd21c7babbbf2f321c937ef714cc0c51
Rafał Machnik <rafal.machnik@uj.edu.pl>
2015-11-13 13:47:37 +0100

    Update ScheduleSystem.java
    adding comparable change

--- zad2/src/main/java/ScheduleSystem.java
+++ zad2/src/main/java/ScheduleSystem.java
@@ -2,7 +2,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 class ScheduleSystem implements SystemInterface {
     private List<Executor> executors = new ArrayList<>();
@@ -16,27 +18,61 @@ class ScheduleSystem implements SystemInterface {
             throw new RuntimeException("Queues count is greater than maxThreadsPerQueue length. " +
                     "Please provide missing configuration.");
         }
-        Arrays.stream(maximumThreadsPerQueue).forEach((size) -> executors.add(Executors.newFixedThreadPool(size)));
+        Arrays.stream(maximumThreadsPerQueue).forEach((queueSize) -> executors.add(
+                new ThreadPoolExecutor(
+                        1,
+                        queueSize,
+                        3,
+                        TimeUnit.SECONDS,
+                        new PriorityBlockingQueue<>())
+                )
+        );
     }
+
 
     public void addTask(TaskInterface task) {
         if (executors.isEmpty()) {
             throw new RuntimeException("Executors configuration is not provided. Please do correct setup before submitting tasks.");
         }
-
-        executors.get(task.getFirstQueue()).execute(scheduleTask(task, task.getFirstQueue()));
+        executors.get(task.getFirstQueue()).execute(new RunnableTask(task, task.getFirstQueue()));
     }
 
-    private Runnable scheduleTask(TaskInterface task, int queueNumber) {
-        return () -> {
-            TaskInterface nextTask = task.work(queueNumber);
-            System.out.println(String.format("Work for task: %d has been started on queue %d.", task.getTaskID(), queueNumber));
+    private class RunnableTask implements Runnable, Comparable {
+        private TaskInterface task;
+        private int currentQueueNumber;
 
-            if (task.getLastQueue() != queueNumber) {
-                executors.get(queueNumber + 1).execute(scheduleTask(nextTask, queueNumber + 1));
+        public RunnableTask(TaskInterface task, int queueNumber) {
+            this.task = task;
+            this.currentQueueNumber = queueNumber;
+        }
+
+        @Override
+        public void run() {
+            TaskInterface nextTask = task.work(currentQueueNumber);
+            if (task.getLastQueue() > currentQueueNumber) {
+                executors.get(currentQueueNumber + 1).execute(new RunnableTask(nextTask, currentQueueNumber + 1));
             } else {
-                System.out.println(String.format("Computing of task: %d completed on queue: %d.", task.getTaskID(), queueNumber));
+                System.out.println(String.format("Computing of task: %d completed on queue: %d.", task.getTaskID(), currentQueueNumber));
             }
-        };
+        }
+
+        public TaskInterface getTask() {
+            return task;
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            RunnableTask o2 = (RunnableTask) o;
+            if (o2.getTask().keepOrder() == task.keepOrder()) {
+                return Integer.compare(task.getTaskID(), o2.getTask().getTaskID());
+            }
+            if (!task.keepOrder()) {
+                return -1;
+            }
+            if (!o2.getTask().keepOrder()) {
+                return 1;
+            }
+            return 0;
+        }
     }
 }
