3ab3c1436cd8ce428f86d08d69af09a4a6a5e232
Rafal <rafik991@gmail.com>
2015-11-13 20:58:48 +0100

    moving classes

--- zad2/src/test/java/ScheduleSystemTest.java
+++ zad2/src/test/java/ScheduleSystemTest.java
@@ -9,12 +9,12 @@ public class ScheduleSystemTest {
     @Test
     public void shouldWork() throws Exception {
         SystemInterface scheduleSystem = new ScheduleSystem();
-        scheduleSystem.setNumberOfQueues(5);
-        scheduleSystem.setThreadsLimit(new int[]{1, 1, 1, 1, 1});
+        scheduleSystem.setNumberOfQueues(4);
+        scheduleSystem.setThreadsLimit(new int[]{2, 2, 2, 2});
         scheduleSystem.addTask(new TaskSample(0, 2, 1, true));
-        scheduleSystem.addTask(new TaskSample(0, 3, 4, true));
-        scheduleSystem.addTask(new TaskSample(0, 3, 2, false));
-        scheduleSystem.addTask(new TaskSample(0, 1, 3, true));
+        scheduleSystem.addTask(new TaskSample(0, 3, 2, true));
+        scheduleSystem.addTask(new TaskSample(1, 1, 3, true));
+
 
         Thread.sleep(100 * 1000);
     }
