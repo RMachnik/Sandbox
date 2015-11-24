f665c932c73ce1268d26341bab0c65bdc3063611
Rafal <rafik991@gmail.com>
2015-11-24 20:11:42 +0100

    fixing keepOrder flag

--- zad2/src/test/java/ScheduleSystemTest.java
+++ zad2/src/test/java/ScheduleSystemTest.java
@@ -7,9 +7,9 @@ public class ScheduleSystemTest {
     public void shouldWork() throws Exception {
         SystemInterface scheduleSystem = new ScheduleSystem();
         scheduleSystem.setNumberOfQueues(5);
-        scheduleSystem.setThreadsLimit(new int[]{2, 2, 2, 2});
+        scheduleSystem.setThreadsLimit(new int[]{3, 3, 3, 3});
         scheduleSystem.addTask(new TaskSample(0, 4, 1, true));
-        scheduleSystem.addTask(new TaskSample(0, 4, 2, true));
+        scheduleSystem.addTask(new TaskSample(0, 4, 2, false));
         scheduleSystem.addTask(new TaskSample(0, 4, 3, true));
         scheduleSystem.addTask(new TaskSample(1, 4, 4, true));
 
